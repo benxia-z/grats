b86d0a44775dfe177a4849059b89978d6cd8ce34
Benjamin Xia <bzijiexia@gmail.com>
2020-08-14 00:16:13 -0500

    deploy to heroku

--- config.py
+++ config.py
@@ -18,7 +18,9 @@ class Config():
     else:
         QUOTE_OF_THE_DAY = {'quote': 'Cats rule the world.',
                             'author': 'Jim Davis'}
-    
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
